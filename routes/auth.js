f4cc2e164feb3f08971b84d6e87792e979d83a3b
mpmcauliffe <mcauliffe99@gmail.com>
2019-07-02 20:48:51 -0600

    auth middleware and User / Contact models

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,68 @@
-const express       = require('express')
-const router        = express.Router()
+const express                      = require('express')
+const bcrypt                       = require('bcryptjs')
+const jwt                          = require('jsonwebtoken')
+const { check, validationResult, } = require('express-validator')
+
+const auth                         = require('../middleware/auth')
+const User                         = require('../models/User')
+const config                       = require('config')
+const router                       = express.Router()
 
 
 // @route       GET api/auth
 // @desc        GET logged in user
 // @access      Private
-router.get('/', (req, res) => {
-    res.send('get logged in user')
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
 })
 
 // @route       POST api/auth
 // @desc        Auth user & get token
 // @access      Public
-router.post('/', (req, res) => {
-    res.send('log in user')
+router.post('/', [
+        check('email', 'Please include a valid email.').isEmail(),
+        check('password', 'Password is required.').exists()
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) { return res.status(400).json({ errors: errors.array() }) }
+
+        const { email, password, } = req.body
+
+        try {
+            let user = await User.findOne({ email })
+            if (!user) { res.status(400).json({ msg: 'Invalid credentials'}) }
+
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) { return res.status(400).json({ msg: 'Invalid credentials'}) }
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(
+                payload, 
+                config.get('JWT_SECRET'), 
+                {
+                    expiresIn: 360000
+                }, 
+                (err, token) => {
+                    if (err) throw err
+                    res.json({ token })
+                }
+            )
+        } catch (err) {
+            console.error(error.message)
+            res.status(500).send('Server error')
+        }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express   = require('express')
+const express       = require('express')
 
-const app       = express()
-const PORT      = process.env.PORT || 5000
+const connectDB     = require('./config/db')
 
+const app           = express()
+const PORT          = process.env.PORT || 5000
+
+
+connectDB()
+
+app.use(express.json({ extended: false }))
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
