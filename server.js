dc00ef65c510f8d89e2a8b9889652455668e50c2
mpmcauliffe <mcauliffe99@gmail.com>
2019-07-09 18:18:33 -0600

    deployment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express       = require('express')
+const path          = require('path')
 
 const connectDB     = require('./config/db')
 
@@ -18,5 +19,15 @@ app.use('/api/contacts', require('./routes/contacts'))
 app.get('/', (req, res) => {
     res.json({ msg: 'welcome to the contacts app'})
 })
+
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
 
 app.listen(PORT, () => console.log(`Server up on ${PORT}`))
